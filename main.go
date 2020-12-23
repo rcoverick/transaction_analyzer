fb81b31bc54019b39aa4e7f9f209aed781238379
= <=>
2020-12-22 22:35:53 -0600

    Added functionality to load config from file.

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -10,7 +12,11 @@ import (
 // what stats are computed and how they're aggregated,
 // what file(s) to read transactions from, etc..
 type config struct {
-	TransactionsFile string
+	TransactionsFile string `json:"transactionsFile"`
+}
+
+type transaction struct {
+	Date string `json:`
 }
 
 // newConfig returns a new instance of a
@@ -28,11 +34,25 @@ func getConfigs() (*config, error) {
 	config := newConfig()
 	if err != nil {
 		// use sane default configurations
-		return config, nil
+		return config, err
 	} else {
 		defer configFile.Close()
 	}
+	bytes, err := ioutil.ReadAll(configFile)
+
+	if err != nil {
+		return config, err
+	}
+
+	json.Unmarshal(bytes, &config)
+
 	return config, err
+}
+
+// loadTransactions loads the csv transactions from
+// the file specified in the configs.
+func loadTransactions(c *config) ([]transaction, error) {
+	return nil, nil
 }
 
 func main() {
@@ -42,5 +62,12 @@ func main() {
 		fmt.Printf("Using default configurations\n")
 	}
 	fmt.Fprintf(os.Stdout, "Configs: %v", configs)
-
+	transactions, err := loadTransactions(configs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading transactions: %v", err)
+		os.Exit(1)
+	}
+	for i := 0; i < len(transactions); i++ {
+		fmt.Fprintf(os.Stdout, "%v", transactions[i])
+	}
 }
